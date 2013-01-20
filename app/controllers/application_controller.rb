b61aea3f9c53c1dea64c3b9fc06f455d8881834b
Noah Gibbs <noah_gibbs@yahoo.com>
2013-01-20 14:43:59 -0800

    Use strong params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include ActiveModel::ForbiddenAttributesProtection  # TODO: remove for Rails 4.0+
 
   def implementors_only!
     # Soon, redirect non-implementors
