3f7ba8948598a42c547499832cdf49bd7d32bb93
Noah Gibbs <noah_gibbs@yahoo.com>
2013-01-20 16:13:56 -0800

    Show 'no game' screen if there's no game configured

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include ActiveModel::ForbiddenAttributesProtection  # TODO: remove for Rails 4.0+
 
+  def default_world
+    World.where(:is_default => true).first
+  end
+
   def implementors_only!
     # Soon, redirect non-implementors
   end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,4 +1,7 @@
 class PlayersController < ApplicationController
   def index
+    world = default_world
+
+    return render :no_game_configured unless world
   end
 end
