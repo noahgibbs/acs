6759fc2b5024bf258105debcb77adc1336f0eb57
Noah Gibbs <noah_gibbs@yahoo.com>
2013-01-21 22:28:38 -0800

    Add check for no servers running

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,13 @@
 class PlayersController < ApplicationController
   def index
     @world = default_world
-
     return render :no_game_configured unless @world
+
+    # Get current server status
+    @status = ServerStatus.where(:world_id => @world.id).first
+
+    return render :no_server_running unless @status && (Time.now - @status.created) <= 10.seconds
+
+
   end
 end
