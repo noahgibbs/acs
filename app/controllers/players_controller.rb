f2aeacf969c9d9a42dab5528786e8437f09b1d0d
Noah Gibbs <noah_gibbs@yahoo.com>
2013-01-22 22:20:21 -0800

    Add 'not logged in' view for players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -6,7 +6,9 @@ class PlayersController < ApplicationController
     # Get current server status
     @status = ServerStatus.where(:world_id => @world.id).first
 
-    return render :no_server_running unless @status && (Time.now - @status.created) <= 10.seconds
+    return render :no_server_running unless @status && (Time.now - @status.updated_at) <= 60.seconds
+
+    return render :not_logged_in unless current_user
 
 
   end
